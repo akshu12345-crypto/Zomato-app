d79fdecbe11e880002a595171d0b4f8cc9b7da2a
akshu12345-crypto <akshata.bhimnale@gmail.com>
2023-12-04 16:52:14 +0530

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./Header.scss"
-import Logo from "../../assets/images/Zomato-Logo.png"
+import Logo from "../../assets/images/Zomato-Logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import blackLogo from "../../assets/images/blackLogo.webp";
 
 const Header = () => {
   return (
